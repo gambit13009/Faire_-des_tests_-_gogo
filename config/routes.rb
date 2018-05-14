5a2ce0acbe4d8b212b119dd96070f436e6999882
Votre nom ou pseudo <maxime.malafosse@gmail.com>
2018-05-14 13:11:43 +0200

    bon appetit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root to: "home#index"  #AFAIRE
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  	get 'home/index', to: "home#index"  
+  	get "/" to: "home#index"  
 
 end
